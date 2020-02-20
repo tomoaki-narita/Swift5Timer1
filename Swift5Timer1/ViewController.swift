cf50fb7d87f35e0e7c2a973dea50923eb6756b3f
output <output.org@gmail.com>
2020-02-20 20:50:18 +0900

    2/20/20:50

--- Swift5Timer1/ViewController.swift
+++ Swift5Timer1/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var startButton: UIButton!
+ 
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var timer = Timer()
+    
+    var count = Int()
+    
+    var imageArray = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        count = 0
+        
+        stopButton.isEnabled = true
+        
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerUpdate(){
+        
+        count = count + 1
+        imageView.image = imageArray[count]
+        
+        
     }
 
-
+    @IBAction func start(_ sender: Any  ) {
+        
+        startButton.isEnabled = true
+        
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        startButton.isEnabled = false
+    }
+    
+    
+    
+    
+    
 }
 
